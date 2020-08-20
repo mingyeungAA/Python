8c4b5408673f0993b1eba0239884804dc65521d1
mingg☆ <68313114+mingyeungAA@users.noreply.github.com>
2020-08-20 23:45:30 +0900

    200820
    Python

---
+++ Code/type01.py
@@ -0,0 +1,34 @@
+#number : 숫자
+
+#정수형
+a=100
+print(a)
+print(type(a))
+
+
+print(int(9.8))
+print(int(7/6))
+print(type(int('10')))
+
+
+#실수형
+print("#실수형")
+b=100.1
+print(b)
+print(type(b))
+
+print(float(4))
+print(float(3+2))
+print(type(float('1.2')))
+
+
+#2진수(0b), 8진수(0o), 16진수(0x)
+print('#2진수, 8진수, 16진수')
+c=0b1111
+print(c)
+d=0o77
+print(d)
+e=0xff
+print(e)
+
+
